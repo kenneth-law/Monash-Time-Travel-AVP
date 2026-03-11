6a0670264258eadf0cf7729917a5174503a254ad
klaw0024 <kenluo1113@gmail.com>
2026-03-11 12:54:32 +1100

    Initial Commit

---
+++ Monash Time Travel AVP/Monash_Time_Travel_AVPApp.swift
@@ -0,0 +1,17 @@
+//
+//  Monash_Time_Travel_AVPApp.swift
+//  Monash Time Travel AVP
+//
+//  Created by Ken Law on 11/3/2026.
+//
+
+import SwiftUI
+
+@main
+struct Monash_Time_Travel_AVPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
